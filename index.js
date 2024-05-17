8a3c651fa4cdb767b221f5e8e0e4b11faa5722a3
Vasukiuma1998-xxx <vasukijegan28@gmail.com>
2024-05-17 10:46:10 +0530

    responsive

---
+++ index.js
@@ -0,0 +1,52 @@
+var sidenav=document.getElementById("sidenav")
+var menuicon=document.getElementById("menuicon")
+var closenav=document.getElementById("closenav")
+
+menuicon.addEventListener("click",function(){
+    sidenav.style.left=0
+})
+
+closenav.addEventListener("click",function(){
+    sidenav.style.left="-50%"
+})
+
+
+var offerbar=document.querySelector(".offerbar")
+var close=document.getElementById("close")
+
+close.addEventListener("click",function(){
+    offerbar.style.display="none"
+})
+
+// slider
+
+currentslideId=1;
+sliderElement = document.getElementById("slider");
+totalSlides=sliderElement.childElementCount;
+console.log(totalSlides)
+
+function next() {
+    if (currentslideId < totalSlides) {
+        currentslideId++;
+        showSlide();
+    }
+}
+function prev(){
+    if(currentslideId>1){
+        currentslideId--;
+        showSlide()
+    }
+}
+
+function showSlide(){
+    slides=document.getElementById("slider").getElementsByTagName("li")
+    for(let index=0; index<totalSlides;index++){
+        const element =slides[index];
+        if(currentslideId==index+1){
+            element.classList.remove("hidden")
+        }
+        else{
+            element.classList.add("hidden")
+        }
+    }
+}
